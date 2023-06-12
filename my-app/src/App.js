eabda447864847cf402ec15ca7a102dbfe2852d0
Ali-Raza4321 <ali.raza001112222@gmail.com>
2023-06-12 15:52:07 +0500

    change component1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Ali from './Ali'
+import Noman from './Noman'
 
 const App = () => {
   return (
     <div>
       <Ali />
+      <Noman />
     </div>
   )
 }
---
+++ my-app/src/Noman.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Noman = () => {
+  return (
+    <div>Noman</div>
+  )
+}
+
+export default Noman
